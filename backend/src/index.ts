3e1de64ec4b38f85fbd4bed6d31d703599e1bb9c
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-26 19:14:00 +0530

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/user/signup", (c) => {
+  return c.json({
+    message: "hey",
+  });
+});
+
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("hi");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("hi");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("hey blog");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("hey...");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("bye....");
+});
+
+export default app;
