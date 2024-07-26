6afceb1df13c525e9e9dd86e757db5ba4e81a492
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-26 22:58:36 +0530

    code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Hello Hono,,,,!");
 });
 
 app.post("/api/v1/user/signup", (c) => {
