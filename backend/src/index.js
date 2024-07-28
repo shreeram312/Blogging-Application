db500ae945cb962814bb7eda05a2cd18360429a5
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-28 21:30:19 +0530

    Signup Route

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { serve } from "@hono/node-server";
+const app = new Hono();
+app.get("/", (c) => {
+    return c.text("Hello Hono,,!");
+});
+app.post("/api/v1/user/signup", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+    const body = await c.req.json();
+    await prisma.user.create({
+        data: {
+            email: body.email,
+            password: body.password,
+        },
+    });
+    return c.text("jo");
+});
+app.post("/api/v1/user/signin", (c) => {
+    return c.text("hi");
+});
+app.post("/api/v1/blog", (c) => {
+    return c.text("hi");
+});
+app.put("/api/v1/blog", (c) => {
+    return c.text("hey blog");
+});
+app.get("/api/v1/blog/:id", (c) => {
+    return c.text("hey...");
+});
+app.get("/api/v1/blog/bulk", (c) => {
+    return c.text("bye....");
+});
+serve(app);
+export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,32 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono,,,,!");
+  return c.text("Hello Hono,,!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
-  return c.json({
-    message: "hey",
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
   });
+
+  return c.text("jo");
 });
 
 app.post("/api/v1/user/signin", (c) => {
