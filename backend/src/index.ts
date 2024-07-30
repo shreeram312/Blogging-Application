a128d01202d060db3e5832c3711d13c6300e222c
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-30 20:37:13 +0530

    Signup & Sugnin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,31 @@ const app = new Hono<{
   };
 }>();
 
+app.use("/api/v1/blog/*", async (c, next) => {
+  //get the header
+  // verify the header
+  //if the header is correct then good to go
+  // else return 403 status code to user
+  const jwt = c.req.header("Authorization");
+  if (!jwt || !jwt.startsWith("Bearer ")) {
+    c.status(403); // You might want to use 401 for unauthorized access
+    return c.json({
+      msg: "No JWT or incorrect format",
+    });
+  }
+
+  const token = jwt.split(" ")[1];
+  console.log(token);
+  const payload = await verify(token, c.env.SECRET);
+  console.log(payload);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set(userId, payload.id);
+  await next();
+});
+
 app.get("/", (c) => {
   return c.text("Hello Hono,,!");
 });
@@ -42,8 +67,31 @@ app.post("/api/v1/user/signup", async (c) => {
   });
 });
 
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("hi");
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({
+      message: "Invalid Creditionals",
+    });
+  }
+
+  const token = await sign({ id: user.id }, c.env.SECRET);
+
+  return c.json({
+    jwt: token,
+  });
 });
 
 app.post("/api/v1/blog", (c) => {
