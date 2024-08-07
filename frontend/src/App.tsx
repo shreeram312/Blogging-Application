08e5fedceb09b3f395c801e80ec04f83811ac492
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-08-07 22:28:19 +0530

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/error" element={<ErrorPage />} /> {/* Error page route */}
